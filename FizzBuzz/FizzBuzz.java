b8b41d907c4d05342475f3afc1e10e59d12a4116
unknown <seanaxn1135@gmail.com>
2025-06-16 17:42:41 +0800

    Add FizzBuzz code kata

--- FizzBuzz/FizzBuzz.java
+++ FizzBuzz/FizzBuzz.java
@@ -1,5 +1,24 @@
-class FizzBuzz {
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println(fizzBuzz());
+    }
+
+    public static List<String> fizzBuzz(){
+        List<String> res = new ArrayList<>();
+        for (int i = 1; i <= 100; i ++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                res.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                res.add("Fizz");
+            } else if (i % 5 == 0) {
+                res.add("Buzz");
+            } else {
+                res.add(String.valueOf(i));
+            }
+        }
+        return res;
     }
 }
--- FizzBuzz/FizzBuzzTest.java
+++ FizzBuzz/FizzBuzzTest.java
@@ -1,4 +1,55 @@
-class FizzBuzzTest {
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
+class FizzBuzzTest {
+    public final FizzBuzz fizzBuzz = new FizzBuzz();
+
+    @Test
+    public void fizzBuzz_shouldReturnListOf1To100() {
+        List<String> fizzBuzzList= FizzBuzz.fizzBuzz();
+        assertEquals(100, fizzBuzzList.size());
+    }
+
+    @Test
+    public void fizzBuzz_shouldBeFizzForMultiplesOfThree() {
+        List<String> fizzBuzzList = FizzBuzz.fizzBuzz();
+        for (int i = 0; i < fizzBuzzList.size(); i ++) {
+            int number = i + 1;
+            String actualOutput = fizzBuzzList.get(i);
+
+            if (number % 3 == 0 && number % 5 != 0) {
+                assertEquals("Fizz", actualOutput);
+            }
+        }
+    }
+
+    @Test
+    public void fizzBuzz_shouldBeBuzzForMultiplesOfFive() {
+        List<String> fizzBuzzList = FizzBuzz.fizzBuzz();
+        for (int i = 0; i < fizzBuzzList.size(); i ++) {
+            int number = i + 1;
+            String actualOutput = fizzBuzzList.get(i);
+
+            if (number % 5 == 0 && number % 3 != 0) {
+                assertEquals("Buzz", actualOutput);
+            }
+        }
+    }
+
+    @Test
+    public void fizzBuzz_shouldBeFizzBuzzForMultiplesOfThreeAndFive() {
+        List<String> fizzBuzzList = FizzBuzz.fizzBuzz();
+        for (int i = 0; i < fizzBuzzList.size(); i ++) {
+            int number = i + 1;
+            String actualOutput = fizzBuzzList.get(i);
+
+            if (number % 5 == 0 && number % 3 == 0) {
+                assertEquals("FizzBuzz", actualOutput);
+            }
+        }
+    }
 
 }
